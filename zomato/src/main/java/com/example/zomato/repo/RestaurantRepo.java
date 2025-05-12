f4a33b11da079b6ca3a7c1558112d98c668963d1
Madhav <94641232+Madhavdua@users.noreply.github.com>
2025-05-12 15:20:51 +0530

    Delete zomato directory

--- zomato/src/main/java/com/example/zomato/repo/RestaurantRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.zomato.repo;
-
-import com.example.zomato.entity.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepo extends JpaRepository<Restaurant,Integer> {
-    public Restaurant findByName(String name);
-}
